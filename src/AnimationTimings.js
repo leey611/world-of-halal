d0d13869b1ea272082874e26391cabea2322d9c9
Joyce Z <yz4867@nyu.edu>
2023-12-09 15:27:24 -0500

    Revert "css change - scale down early"
    This reverts commit 7aad780425b1c37ffc70f6025fad365d2f656dea.

--- src/AnimationTimings.js
+++ src/AnimationTimings.js
@@ -1,5 +1,4 @@
 export const AnimationTimings = {
-<<<<<<< Updated upstream
     Start: 120,
     PhotoScale: 50,
     PhotoFade: 50,
@@ -14,21 +13,5 @@ export const AnimationTimings = {
     FadeSphereIn: 50,
     End: 40,
 }
-=======
-  Start: 60,
-  PhotoScale: 50,
-  PhotoFade: 50,
-  Camera1: 30,
-  Camera2: 30,
-  Camera3: 30,
-  Camera4: 30,
-  Camera5: 30,
-  Wait1: 300,
-  FadeCartIn: 50,
-  ZoomInCart: 30,
-  FadeSphereIn: 50,
-  End: 260,
-};
->>>>>>> Stashed changes
 
 // NOTE: This should total 1000. Each unit represents 1vh (since our entire article is 1000vh in height total)
