af3f3289d0b15ade7bd185cd956c5cd36c73adc6
yu <43624848+leey611@users.noreply.github.com>
2023-12-11 16:26:47 -0500

    config base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    base: '/world-of-halal',
+    base: '/world-of-halal/',
     port: 3000,
   },
   plugins: [react()],
