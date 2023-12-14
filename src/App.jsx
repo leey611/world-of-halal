f5d15a8941d5cddca5dc87cd42dfc245a91653dc
Joyce Z <yz4867@nyu.edu>
2023-12-14 16:53:58 -0500

    tag position

--- src/App.jsx
+++ src/App.jsx
@@ -229,7 +229,7 @@ function App() {
       <div
         className="scrollPrompt"
         style={{
-          top: `1030vh`,
+          top: `1070vh`,
         }}
       >
         Scroll down to see Ayman's Cart in 360° <span id="scrollArrow">↓</span>
