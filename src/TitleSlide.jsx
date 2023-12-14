229d21f834ec375efc518de7c061925a58d022a9
yu <43624848+leey611@users.noreply.github.com>
2023-12-14 00:01:26 -0500

    fix credit jsx and rwd css

--- src/TitleSlide.jsx
+++ src/TitleSlide.jsx
@@ -3,11 +3,13 @@ import "./App.css"
 function TitleSlide(props){
   const { children, viewportPosition, title, subtitle, credits } = props;
   
-  return (<div className="titleSlide" style={{position: "absolute", paddingLeft: "20px", top: `${viewportPosition}vh`, zIndex: "50"}}>
+  return (<>
+    <div className="titleSlide" style={{position: "absolute", top: `${viewportPosition}vh`, zIndex: "50"}}>
       <div className="title">{title}</div>
       <div className="subtitle">{subtitle}</div>
-      <div className="credits">{credits}</div>
     </div>
+    {credits && <div className="credits">{credits}</div>}
+  </>
   )
 }
 
